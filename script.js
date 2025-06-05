7cb9ea3fbf8bee644353f15ba8146c75e9873492
Anslem Coelho <anslem1999@gmail.com>
2025-06-05 18:14:54 -0400

    Fixed code

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 
-let count = 0
+let count = 0;
 
 function increase() {
-    count = count + 1
-    updateCounter()
-    console.log("Counter increased")
+    count = count + 1;
+    updateCounter();
+    console.log("Counter increased");
 }
 
 function decrease() {
-    count = count - 1
-    updateCounter()
-    console.log("Counter decreased")
+    count = count - 1;
+    updateCounter();
+    console.log("Counter decreased");
 }
 
 function reset() {
-    count == 0
-    updateCounter()
-    console.log("Counter reset")
+    count = 0;
+    updateCounter();
+    console.log("Counter reset");
 }
 
 function updateCounter() {
-    document.getElementById('count').text = count
+    document.getElementById('count').innerHTML = count;
 }
 
 document.getElementById('greetBtn').addEventListener("click", function() {
     const name = document.getElementById("name").value
-    if(name = "") {
-        alert("Please enter your name")
+    if(name == "") {
+        alert("Please enter your name");
     } else {
-        document.getElementById("welcome").innerText = "Welcome, " + name
+        document.getElementById("welcome").innerHTML = "Welcome, " + name;
     }
 })
